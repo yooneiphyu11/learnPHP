62c71354c0b1fafa5482b87c98e8e28e0d59ab01
yooneiphyu11 <yphyu49@gmail.com>
2024-05-16 21:36:17 +0630

    array.php

---
+++ array.php
@@ -0,0 +1,27 @@
+<?php
+ 
+ $info1 = [1,2,3,4,5,6,7,8,9,10];
+ echo "<pre/>";
+ var_dump($info1);
+ var_dump($info1[4]);
+ echo "<br>";
+
+ $info2 = [
+    "name" => "Yoon",
+    "age" => 20,
+    "Father" => "U Myint",
+    "Mother" => "Daw Khin",
+    "Height" => "165cm",
+    "Blood type" => "O",
+    "Relationship" => "couple",
+    "Education " => "uni student",
+    "The holidays before school" => 15,
+    "Year" => "3rd year"
+ ];
+
+echo "<pre/>";
+var_dump($info2);
+var_dump($info2["Height"]);
+echo "<br>";echo "<pre/>";
+var_dump($data);
+echo "<br>";
